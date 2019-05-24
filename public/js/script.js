8d3bc4102ab4a418d61589e27289eb6ccc6bedc8
Julian <julianbgs96@gmail.com>
2019-05-24 17:08:47 +0300

    finish adaptive

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+//mobile menu
+    $(document).on('click', '#header-trigger', function (event) {
+        event.preventDefault();
+        var menu = $('.header__menu');
+        if ($(this).hasClass('js-trigger-active')) {
+            $(this).removeClass('js-trigger-active');
+            menu.slideUp('slow');
+        } else {
+            $(this).addClass('js-trigger-active');
+            menu.slideDown('slow');
+        }
+        return false;
+    });
+  //close script
+});
+
